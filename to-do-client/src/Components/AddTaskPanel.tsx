31482d12ddcd093a15569ad4389dde5b5ec0eaac
Kamil Marczak <kamil.codemate@gmail.com>
2023-11-17 14:20:27 +0100

    Made a part of task adding form

--- to-do-client/src/Components/AddTaskPanel.tsx
+++ to-do-client/src/Components/AddTaskPanel.tsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BsFillSunFill, BsExclamationDiamondFill, BsPlusSquareFill } from 'react-icons/bs';
-import { GrSchedule } from 'react-icons/gr';
-import { IoCheckmarkDoneSharp } from 'react-icons/io5';
-import { AiFillHome } from 'react-icons/ai';
+import './ComponentStyles/AddTaskPanel.scss';
 type Props = {
   username: string;
   token: string;
@@ -12,7 +9,35 @@ type Props = {
 const AddTaskPanel: React.FC<Props> = ({ username, token }): React.ReactElement => {
   return (
     <div className='add-task-panel-container'>
-      <div className='add-task-panel'></div>
+      <header>Add New Task</header>
+      <div className='add-task-area-container'>
+        <div className='add-task-panel'>
+          <form className='add-task-form'>
+            <div className='add-task-section-left'>
+              <div className='label-input-section'>
+                <label>Name: </label> <input type='text' name='name' className='add-task-input' />
+              </div>
+              <div className='label-input-section'>
+                <label>Description:</label>
+                <textarea name='description' className='textarea-desc'></textarea>
+              </div>
+              <div className='label-input-section'>
+                <label>Date: </label>
+                <input type='date' name='date' className='add-task-datetime' />
+              </div>
+              <div className='label-input-section'>
+                <label>Start time:</label>
+                <input type='time' className='add-task-datetime' />
+              </div>
+              <div className='label-input-section'>
+                <label>End time:</label>
+                <input type='time' className='add-task-datetime' />
+              </div>
+            </div>
+            <div className='add-task-section-right'></div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
